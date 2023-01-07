bd6687dd19349cc07f0754364b5d0c5d84705cbf
Alexander Ivanov <sasha2108i@gmail.com>
2023-01-07 13:15:13 +0300

    fix: update task 9

--- 9/main.cpp
+++ 9/main.cpp
@@ -6,19 +6,19 @@ int main(){
 	srand((unsigned) time(0)); // for random nums
 
 	int arraySizeY, arraySizeX;
-	int nums[arraySizeY][arraySizeX];
 	cout << "Print X: ";
 	cin >> arraySizeX;
 	cout << "Print Y: ";
 	cin >> arraySizeY;
-	
+	int nums[arraySizeY][arraySizeX];
+
 	for (int i = 0; i < arraySizeY; ++i) { // giving random values for matrix
 		for (int j = 0; j < arraySizeX; ++j) {
-			nums[i][j] = (1 + (rand() % 10));
+			nums[i][j] = 1 + (rand() % 10);
 		}
 	}
 	
-	int randomVal = 1 + (rand() % arraySizeX);
+	int randomVal = (rand() % arraySizeX);
 
 	for (int i = 0; i < arraySizeY; ++i) { 
 			nums[i][randomVal] = 0;	 // counter for giving 1 zero on one line
